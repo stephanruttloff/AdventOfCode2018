5e77b2d885ddb4a48a41b76a32ba9d73a0921cc1
Stephan Ruttloff <stephanruttloff@users.noreply.github.com>
2018-12-06 13:55:38 +0100

    Finished first assignment of day 2

---
+++ Day2_Part1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counts =
+                File
+                    .ReadAllLines(@"input.txt")
+                    .Select(x =>
+                    {
+                        var grouped = x.GroupBy(y => y).ToList();
+
+                        return new
+                        {
+                            Doubles = grouped.Any(y => y.Count() == 2),
+                            Triples = grouped.Any(y => y.Count() == 3)
+                        };
+                    })
+                    .ToList();
+
+            var doubleCount = counts.Count(x => x.Doubles);
+            var tripleCount = counts.Count(x => x.Triples);
+            var result = doubleCount * tripleCount;
+
+            Console.WriteLine($"{doubleCount} * {tripleCount} = {result}");
+            Console.ReadKey();
+        }
+    }
+}
