4ceb768b3e8b08bc691fa7ecd50fda8b762a07c7
Stephan Ruttloff <stephanruttloff@users.noreply.github.com>
2018-12-06 16:55:47 +0100

    Finished second assignment of day 3

---
+++ Day3_Part2/Claim.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace Day3_Part2
+{
+    internal class Claim
+    {
+        #region Constants
+
+        private const string Regex_Entry = @"^#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)$";
+
+        #endregion
+
+        #region Properties
+
+        public int Id { get; }
+        public string Entry { get; }
+        public Rectangle Rectangle { get; }
+
+        #endregion
+
+        #region Constructor
+
+        public Claim(string entry)
+        {
+            var match = Regex.Match(entry, Regex_Entry);
+
+            if(!match.Success)
+                throw new ArgumentException($"Invalid input: {entry}");
+
+            Id = int.Parse(match.Groups[1].Value);
+            Entry = entry;
+            var x = int.Parse(match.Groups[2].Value);
+            var y = int.Parse(match.Groups[3].Value);
+            var width = int.Parse(match.Groups[4].Value);
+            var height = int.Parse(match.Groups[5].Value);
+            Rectangle = new Rectangle(x, y, width, height);
+        }
+
+        #endregion
+    }
+}
---
+++ Day3_Part2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day3_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var claims = File.ReadAllLines(@"input.txt").Select(x => new Claim(x)).ToList();
+            var intersecting = new List<Claim>();
+
+            for (var i = 0; i < claims.Count - 1; i++)
+            for (var j = i + 1; j < claims.Count; j++)
+            {
+                if (claims[i].Rectangle.IntersectsWith(claims[j].Rectangle))
+                {
+                    intersecting.Add(claims[i]);
+                    intersecting.Add(claims[j]);
+                }
+            }
+
+            var remaining = claims.Except(intersecting).Single();
+
+            Console.WriteLine($"The one good claim: #{remaining.Id}");
+            Console.ReadKey();
+        }
+    }
+}
