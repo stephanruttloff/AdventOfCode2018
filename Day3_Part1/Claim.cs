353734796c6363efdd39e53348fe6262fa2e105d
Stephan Ruttloff <stephanruttloff@users.noreply.github.com>
2018-12-06 16:39:26 +0100

    Finished first assignment of day 3

---
+++ Day3_Part1/Claim.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day3_Part1
+{
+    internal class Claim
+    {
+        #region Constants
+
+        private const string Regex_Entry = @"^#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)$";
+
+        #endregion
+
+        #region Properties
+
+        public int Id { get; }
+        public string Entry { get; }
+        public int X { get; }
+        public int Y { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public int RightEdge => X + Width;
+        public int BottomEdge => Y + Height;
+
+        #endregion
+
+        #region Constructor
+
+        public Claim(string entry)
+        {
+            var match = Regex.Match(entry, Regex_Entry);
+
+            if(!match.Success)
+                throw new ArgumentException($"Invalid input: {entry}");
+
+            Id = int.Parse(match.Groups[1].Value);
+            Entry = entry;
+            X = int.Parse(match.Groups[2].Value);
+            Y = int.Parse(match.Groups[3].Value);
+            Width = int.Parse(match.Groups[4].Value);
+            Height = int.Parse(match.Groups[5].Value);
+        }
+
+        #endregion
+    }
+}
---
+++ Day3_Part1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day3_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var claims = File.ReadAllLines(@"input.txt").Select(x => new Claim(x)).ToList();
+            var fabricWidth = claims.Max(x => x.RightEdge);
+            var fabricHeight = claims.Max(x => x.BottomEdge);
+            var fabric = new uint[fabricWidth, fabricHeight];
+            var overlapAmount = 0;
+
+            foreach (var claim in claims)
+            {
+                for(var x = claim.X; x < claim.RightEdge; x++)
+                for (var y = claim.Y; y < claim.BottomEdge; y++)
+                {
+                    fabric[x, y]++;
+                    if (fabric[x, y] == 2)
+                        overlapAmount++;
+                }
+            }
+            
+            Console.WriteLine($"Overlapping: {overlapAmount}");
+            Console.ReadKey();
+        }
+    }
+}
